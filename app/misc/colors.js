409f7d06c1301411fa5d83975c431e4fd87a0eeb
Bruno Bianchi Pagani <93994299+Godogani@users.noreply.github.com>
2022-08-23 13:44:52 -0300

    Add files via upload

---
+++ app/misc/colors.js
@@ -0,0 +1,9 @@
+//Aqui so colocamos as cores, primeiro usamos a palavra chave padrao de exportação para exportar este objeto, dentro desse
+//objeto, temos esses pares de valores-chave que são simplesmente corres.
+
+export default {
+  DARK: '#1e1e1e',
+  LIGHT: '#ffffff',
+  PRIMARY: '#6272a4',
+  ERROR: '#ff0000',
+};
